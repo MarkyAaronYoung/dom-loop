f4427dbea29fcb4793c1c14f9929bce912f7768c
Mark Young <markyaaronyoung@gmail.com>
2020-04-21 18:55:20 -0500

    loop over colors, create domstring

---
+++ loops.js
@@ -0,0 +1,19 @@
+const colors = ['red', 'orange', 'white', 'blue', 'gold', 'yellow', 'violet'];
+
+
+const colorLoop = () => {
+  let domString = '';
+  for (let i = 0; i < colors.length; i++) {
+    // console.log(colors[i]);
+    domString += `<h2>${colors[i]}</h2>`;
+    // console.log(domString);
+  }
+   // return domString;
+  console.log(domString);
+};
+
+const init = () => {
+  colorLoop();
+};
+
+init();
